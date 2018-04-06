97181b3b3074ec2102d4eb8b07ec4f2a31548fc8
Federico Ponchio <ponchio@gmail.com>
2018-04-06 18:35:57 +0200

    autodetect of layout tilesize and overlap

--- test/build_deepzoom.sh
+++ test/build_deepzoom.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for i in plane*.jpg; do 
 	rm -rf ${i%.jpg}_files;
-	vips dzsave $i ${i%.jpg} --layout dz --tile-size 256 --overlap 8  --depth onetile --suffix .jpeg[Q=95]; 
+	rm -rf ${i%.jpg};
+	vips dzsave $i ${i%.jpg} --layout dz --tile-size 256 --overlap 8  --depth onetile --suffix .jpg[Q=95]; 
 done
