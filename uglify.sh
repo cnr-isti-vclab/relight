aa97fd74ed3a5b255534feb7bb5f08e6f1f7103a
ponchio <ponchio@gmail.com>
2018-07-19 18:58:36 +0200

    viewer almost ready

--- uglify.sh
+++ uglify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 uglifyjs relight.js > relight.min.js
-cat relight.min.js >> relight-viewer.min.js
-uglify relight-viewer.js >> relight-viewer.min.js
+cat relight.min.js > relight-viewer.min.js
+uglifyjs relight-viewer.js >> relight-viewer.min.js
