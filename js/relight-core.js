f3a8d9a3cde8bcddf77168119ccc2f81a57aea76
ponchio <ponchio@gmail.com>
2018-11-30 12:49:21 +0100

    initial canvas refactoring

--- js/relight-core.js
+++ js/relight-core.js
@@ -23,8 +23,6 @@ Overlay enlarges the tiles but not the side on the border of the image (need to 
 Options:
 
 	gl: if provided will use an already created context
-
-//TODO This should be per image!
 	url: path of the directory where the json (and the images) resides
 	path: for iip absolute path of the same directory
 	layout: pick between image, deepzoom, google, iip, iiif, zoomify
@@ -38,45 +36,31 @@ Options:
 Methods:
 */
 
-function Relight(item, o) {
+function Relight(gl, options) {
 	var t = this;
-	if(!item)
+	if(!gl)
 		return null;
+	t.gl = gl;
 
 	t.options = Object.assign({
-		layout: "image",
 		visible: true,
-		light: [0, 0, 1],
-		pos: { x: 0, y:0, z:0, a: 0, t:0 },
-		background: [0, 0, 0, 0],
-		bounded: true,
-		border: 1,                   //prefetching tiles out of view
-		maxRequested: 4,
-		fit: true,                   //scale on load.
+		transparency: 1,
+		layout: "image",
+
 		suffix: ".jpg",
-		preserveDrawingBuffer: false,
+		position: [ 0, 0, 0 ],
+		scale: 1,
 		rotation: 0,
-		normals: false
-	}, o);
 
-	t.pos = t.options.pos;
-	if(t.options.rotation)
-		t.pos.a = t.options.rotation;
+		light: [0, 0, 1],
+		normals: false,
+
+		border: 1,                   //prefetching tiles out of view
+		maxRequested: 4
+
+	}, options);
+
 	t.normals = t.options.normals;
-
-	if(typeof(item) == 'string')
-		item = document.querySelector(item);
-	if(item.tagName != "CANVAS")
-		return null;
-	t.canvas = item;
-
-
-	var glopt = { antialias: false, depth: false, preserveDrawingBuffer: t.options.preserveDrawingBuffer };
-	t.gl = o.gl || t.canvas.getContext("webgl2", glopt) || 
-			t.canvas.getContext("webgl", glopt) || 
-			t.canvas.getContext("experimental-webgl", glopt) ;
-	if (!t.gl) return null;
-
 
 	if(t.url && t.url.endsWidth("/"))
 		t.url = r.url.slice(0, -1);
@@ -84,7 +68,7 @@ function Relight(item, o) {
 	for(var i in t.options)
 		t[i] = t.options[i];
 
-	t.previous = { x:0, y:0, z:0, t:0 };
+
 	t.previousbox = [1, 1, -1, -1];
 
 	t.nodes = [];
@@ -101,14 +85,13 @@ function Relight(item, o) {
 	t._onposchange = [];
 	t._onlightchange = [];
 
-	t.initGL();
+
 
 	if(t.img) { //this meas we are loading an image
 		t.loadInfo({type: 'img', colorspace: null, width: 0, height: 0, nplanes: 3 });
 	} else if(t.url) {
 		t.setUrl(t.url);
 	}
-
 	return this;
 }
 
@@ -225,13 +208,6 @@ loadInfo: function(info) {
 	t.loaded();
 },
 
-onPosChange: function(f) {
-	this._onposchange.push(f);
-},
-
-onLightChange: function(f) {
-	this._onlightchange.push(f);
-},
 
 onLoad: function(f) {
 	this._onload.push(f);
@@ -404,13 +380,6 @@ initTree: function() {
 },
 
 
-resize: function(width, height) {
-	this.canvas.width = width;
-	this.canvas.height = height;
-	this.prefetch();
-	this.redraw();
-},
-
 rot: function(dx, dy, a) {
 	var a = Math.PI*(a/180);
 	var x =  Math.cos(a)*dx + Math.sin(a)*dy;
@@ -418,21 +387,7 @@ rot: function(dx, dy, a) {
 	return [x, y];
 },
 
-project: function(pos, x, y) { //convert image coords to canvas coords.
-	var t = this;
-	var r = t.rot(x - pos.x,  y - pos.y, pos.a);
-	var z = Math.pow(2, pos.z);
-	return [r[0]/z + t.canvas.width/2, r[1]/z + t.canvas.height/2]
-},
 
-iproject: function(pos, x, y) {
-	var t = this;
-	var z = Math.pow(2, pos.z);
-	x = (x - t.canvas.width/2)*z;
-	y = (y - t.canvas.height/2)*z;
-	[x, y] = t.rot(x, y, -pos.a);
-	return [x + pos.x, y + pos.y];
-},
 
 getBox: function(pos) {
 	var t = this;
@@ -461,111 +416,6 @@ getIBox: function(pos) {
 	}
 	return box;
 },
-
-
-zoom: function(dz, dt) {
-	var p = this.pos;
-	this.setPosition(dt, p.x, p.y, p.z+dz, p.a);
-},
-
-center: function(dt) {
-	var p = this.pos;
-	this.setPosition(dt, this.width/2, this.height/2, p.z, p.a);
-},
-
-centerAndScale: function(dt) {
-	var t = this;
-	t.pos.x = t.width/2;
-	t.pos.y = t.height/2;
-	t.pos.z = 0;
-	var box = t.getBox(t.pos);
-	var scale = Math.max((box[2]-box[0])/t.canvas.width, (box[3]-box[1])/t.canvas.height);
-	var z = Math.log(scale)/Math.LN2;
-	this.setPosition(dt, t.pos.x, t.pos.y, z, t.pos.a);
-},
-
-pan: function(dt, dx, dy) { //dx and dy expressed as pixels in the current size!
-	var p = this.pos;
-	//size of a rendering pixel in original image pixels.
-	var scale = Math.pow(2, p.z);
-	var r = this.rot(dx, dy, p.a);
-	this.setPosition(dt, p.x - r[0]*scale, p.y - r[1]*scale, p.z, p.a);
-},
-
-rotate: function(dt, angle) {
-	var p = this.pos;
-	var a = p.a + angle;
-	while(a > 360) a -= 360;
-	while(a <   0) a += 360;
-	this.setPosition(dt, p.x, p.y, p.z, a);
-},
-
-setPosition: function(dt, x, y, z, a) {
-
-	var t = this;
-	var scale = Math.pow(2, z);
-
-	if(0 && t.bounded && t.width) {
-		var zx = Math.min(z, Math.log(t.width/t.canvas.width)/Math.log(2.0));
-		var zy = Math.min(z, Math.log(t.height/t.canvas.height)/Math.log(2.0));
-		z = Math.max(zx, zy);
-		scale = Math.pow(2, z);
-
-		var ix = [scale*t.canvas.width/2, t.width - scale*t.canvas.width/2].sort(function(a, b) { return a-b; });
-		x = Math.max(ix[0], Math.min(ix[1], x));
-		var iy = [scale*t.canvas.height/2, t.height - scale*t.canvas.height/2].sort(function(a, b) { return a-b; });
-		y = Math.max(iy[0], Math.min(iy[1], y));
-
-		if(z <= -1) z = -1;
-	}
-
-	if(!dt) dt = 0;
-	var time = performance.now();
-	t.previous = t.getCurrent(time);
-
-	if(x == t.previous.x && y == t.previous.y && z == t.previous.z && a == t.previous.a)
-		return;
-
-	t.pos = { x:x, y:y, z:z, a:a, t:time + dt };
-
-	t.prefetch();
-	t.redraw();
-
-	for(var i = 0; i < t._onposchange.length; i++)
-		t._onposchange[i]();
-},
-
-getCurrent: function(time) {
-	var t = this;
-
-	if(time > t.pos.t)
-		return { x: t.pos.x, y: t.pos.y, z: t.pos.z, a: t.pos.a, t: time };
-
-	var dt = t.pos.t - t.previous.t;
-	if(dt < 1) return t.pos;
-
-	var dt = (t.pos.t - time)/(t.pos.t - t.previous.t); //how much is missing to pos
-	var ft = 1 - dt;
-
-	return { 
-		x:t.pos.x*ft + t.previous.x*dt, 
-		y:t.pos.y*ft + t.previous.y*dt, 
-		z:t.pos.z*ft + t.previous.z*dt, 
-		a:t.pos.a*ft + t.previous.a*dt,
-		t:time };
-},
-
-
-initGL: function() {
-	var gl = this.gl;
-	gl.pixelStorei(gl.UNPACK_COLORSPACE_CONVERSION_WEBGL, gl.NONE);
-	var b = this.options.background;
-	gl.clearColor(b[0], b[1], b[2], b[3], b[4]);
-	gl.disable(gl.DEPTH_TEST);
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-},
-
 
 
 // p from 0 to nplanes,
@@ -1133,11 +983,7 @@ draw: function(timestamp) {
 	gl.disable(gl.SCISSOR_TEST);
 }, 
 
-redraw: function() {
-	var t = this;
-	if(t.animaterequest) return;
-	t.animaterequest = requestAnimationFrame(function (time) { t.draw(time); });
-},
+redraw: function() {}, //placeholder for other to replace the draw code.
 
 prefetch: function() {
 	var t = this;
