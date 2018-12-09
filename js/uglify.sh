c385fc6dc659d44b2126895a408560cbee7d3ff7
Federico Ponchio <ponchio@gmail.com>
2018-12-09 12:07:43 +0100

    using terser instead of uglifyjs

---
+++ js/uglify.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./uglifyjs relight-core.js > relight.min.js
+./uglifyjs relight-shaders.js >> relight.min.js
+cat relight.min.js > relight-viewer.min.js
+./uglifyjs relight-interface.js >> relight-viewer.min.js
+./uglifyjs hammer.js > hammer.min.js
+
