0ca3ff2ec0a70fe35324f15f3324d81adb61f644
ponchio <ponchio@gmail.com>
2021-03-26 05:20:43 +0100

    added tarzoom export

---
+++ scripts/build_zoomify.py
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in plane*.jpg; do 
+	rm -rf ${i%.jpg}_files;
+	rm -rf ${i%.jpg};
+	vips dzsave $i ${i%.jpg} --layout zoomify --tile-size 256 --overlap 8  --depth onetile --suffix .jpg[Q=95]; 
+done
