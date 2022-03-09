90d14854f6e71b5f3f175fa56727d7a463229579
Nicola <47360416+unsettledgames@users.noreply.github.com>
2022-03-09 09:10:42 +0100

    Added ThreadPool, fixed Release build errors

--- relight/normalstask.cpp
+++ relight/normalstask.cpp
@@ -14,14 +14,6 @@
 #include <iostream>
 #include <time.h>
 
-/**TODO
- * OPTIMIZATIONS
- * - Trashing happens after a few lines?
- * - Pass multiple lines, try to process them all in the same system, copy
- *   normals at the end. The amount of lines should depend on the available cache.
- * - Cycle by column, not by row
- */
-
 void NormalsTask::run()
 {
     switch (m_Method)
@@ -43,12 +35,10 @@ void NormalsTask::run()
 
 /**TODO:
  * - support cropping
- *
  */
 void NormalsTask::solveL2()
 {
     int start = clock();
-    //qDebug() << "Task started";
     // Pixel data
     Eigen::MatrixXd mLights(m_Lights.size(), 3);
     Eigen::MatrixXd mPixel(m_Lights.size(), 1);
@@ -59,19 +49,15 @@ void NormalsTask::solveL2()
     // Fill the lights matrix
     for (int i=0; i<m_Lights.size(); i++)
         for (int j=0; j<3; j++)
-            mLights(i, j) = m_Lights.at(i)[j];
+            mLights(i, j) = m_Lights[i][j];
 
     // For each pixel in the line solve the system
     for (int p=0; p<m_Row.size(); p++)
     {
-        // Get the color data of the pixel for all the nLights images
-        std::vector<Color3f> color = m_Row.pixel(p);
         // Fill the pixel vector
-        for (int m=0; m<color.size(); m++)
-        {
-            Color3f currColor = color[m].clip();
-            mPixel(m, 0) = currColor.mean();
-        }
+        for (int m=0; m<m_Lights.size(); m++)
+            mPixel(m, 0) = m_Row[p][m].mean();
+
         // Solve
         mNormals = (mLights.transpose() * mLights).ldlt().solve(mLights.transpose() * mPixel);
         mNormals.col(0).normalize();
@@ -85,7 +71,7 @@ void NormalsTask::solveL2()
     }
 
     int end = clock();
-    //qDebug() << "TIME: " << ((float)(end - start) / CLOCKS_PER_SEC);
+    qDebug() << "TIME: " << ((float)(end - start) / CLOCKS_PER_SEC);
 }
 
 void NormalsTask::solveSBL()
