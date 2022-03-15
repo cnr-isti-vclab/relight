c16a229c7a474cbc0f762dec699d0c1ff4dc8243
Nicola <47360416+unsettledgames@users.noreply.github.com>
2022-03-15 12:43:57 +0100

    Finished basic UI, started binding events

--- relight/zoom.cpp
+++ relight/zoom.cpp
@@ -4,6 +4,15 @@
 Zoom::Zoom(QWidget* parent) : QDialog(parent), m_Ui(new Ui::ZoomDialog)
 {
     m_Ui->setupUi(this);
+
+    // Deep zoom
+    connect(m_Ui->buttonBrowseRtiDz, SIGNAL(clicked()), this, SLOT(buttonBrowseRtiClicked()));
+    connect(m_Ui->buttonBrowseOutputDz, SIGNAL(clicked()), this, SLOT(buttonBrowseOutputClicked()));
+    connect(m_Ui->inputJpegQuality, SIGNAL(textChanged(const QString&)), this, SLOT(inputQualityChanged(const QString&)));
+
+    connect(m_Ui->buttonConfirm, SIGNAL(clicked()), this, SLOT(buttonConfirmClicked()));
+
+    // Tarzoom
 }
 
 void Zoom::setTabIndex(int index)
--- relight/zoom.h
+++ relight/zoom.h
@@ -7,15 +7,42 @@
 
 class Zoom : public QDialog
 {
+    enum class ZoomType { DeepZoom = 0, Tarzoom = 1, ITarzoom = 2 };
+
     Q_OBJECT
 public:
     explicit Zoom(QWidget* parent = nullptr);
     ~Zoom(){};
 
+    void doZoom();
+
     void setTabIndex(int index);
+    void setZoomType(ZoomType type);
+
+    void setOutputFolder(QString output) {m_OutputFolder = output;}
+    void setInputFolder(QString input) {m_InputFolder = input;}
+    void setJpegQuality(int quality) {m_JpegQuality = quality;}
+    void setOverlap(int overlap) {m_Overlap = overlap;}
+    void setTileSize(int tilesize) {m_TileSize = tilesize;}
+
+private slots:
+    void buttonBrowseOutputClicked();
+    void buttonBrowseRtiClicked();
+    void buttonConfirmClicked();
+
+    void inputQualityChanged(QString& newValue);
+    void inputTileSizeChanged(QString& newValue);
+    void inputOverlapChanged(QString& newValue);
 
 private:
     Ui::ZoomDialog* m_Ui;
+
+    QString m_OutputFolder;
+    QString m_InputFolder;
+
+    int m_JpegQuality;
+    int m_Overlap;
+    int m_TileSize;
 };
 
 #endif // ZOOM_H
