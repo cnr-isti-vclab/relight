b57bad34f708af12ef476094516460e539fceba4
Nicola <47360416+unsettledgames@users.noreply.github.com>
2022-04-12 12:02:51 +0200

    Almost finished single image dstretch
    Should fix burnt areas.

--- relight/dstretchtask.cpp
+++ relight/dstretchtask.cpp
@@ -42,89 +42,105 @@ void DStretchTask::run()
 
     // Covariance matrix
     Eigen::MatrixXd covariance(3, 3);
-    // Correlation matrix
-    Eigen::MatrixXd correlation(3, 3);
     // Channel means
     Eigen::VectorXd means(3);
     means.fill(0);
 
     // TODO: make sure at least nSamples are sampled
-    int samplesPerLine = 5;
+    uint32_t samplesHorizontal = std::ceil(std::sqrt(1000) * ((float)width / height));
+    uint32_t samplesVertical = std::ceil(std::sqrt(1000) * ((float)height / width));
+    uint32_t rowSkip = height / samplesVertical;
+    uint32_t colSkip = width / samplesHorizontal;
+
     // Sample each line
     for (int row=0; row<height; row++)
     {
         // Read the line
         decoder.readRows(1, pixels.data());
 
-        // Getting the samples
-        for (int col=0; col<pixels.size(); col+=samplesPerLine*3)
+        if (row % rowSkip == 0)
         {
-            for (int i=0; i<samplesPerLine; i++)
+            qDebug() << "Row: " << row;
+            // Getting the samples
+            for (int col=0; col<pixels.size(); col+=colSkip*3)
             {
-                means(0) += pixels[col+i];
-                means(1) += pixels[col+i +1];
-                means(2) += pixels[col+i +2];
+                means(0) += pixels[col];
+                means(1) += pixels[col +1];
+                means(2) += pixels[col +2];
+
+                Color3b color(pixels[col], pixels[col+1], pixels[col+2]);
+                samples.push_back(color);
             }
-            samples.push_back({pixels[col], pixels[col+1], pixels[col+2]});
         }
 
         progressed("Sampling...", (row * 100) / width);
     }
 
-    // Compute the covariance matrix
-    for (int l=0; l<3; l++)
-    {
-        for (int m=0; m<3; m++)
-        {
-            unsigned long long sumX = 0, sumM = 0, sumL = 0;
-            for (int k=0; k<samples.size(); k++)
-            {
-                sumL += samples[k][l];
-                if (m <= l)
-                    sumM = sumL;
-                sumX += sumL * samples[k][m];
-            }
-            covariance(l,m) = (1.0f/(samples.size()-1)) * (sumX - (1.0f/samples.size()) * sumL * sumM);
-            progressed("Computing covariance...", (((l+1)*(m+1)+m+1) * 100) / 9);
-        }
-    }
+    // Compute the mean of the channels
+    for (int i=0; i<3; i++)
+        means(i) /= samples.size();
 
-    // Compute the correlation matrix
+    long long sumChannel[]= {0,0,0};
+    long double sumX[][3] = {{0,0,0},{0,0,0},{0,0,0}};
+
+    for (int k=0; k<samples.size(); k++)
+        for (int i=0; i<3; i++)
+            sumChannel[i] += samples[k][i];
+
+    for (int l=0; l<3; l++)
+        for (int m=0; m<3; m++)
+            for (int k=0; k<samples.size(); k++)
+                sumX[l][m] += (samples[k][l] * samples[k][m]) / (samples.size()-1);
+
+    double reducer = ((long double)1.0f/((samples.size()-1) * samples.size()));
     for (int l=0; l<3; l++)
     {
         for (int m=0; m<3; m++)
         {
-            correlation(l,m) = covariance(l,m) / std::sqrt(covariance(l,l) * covariance(m,m));
-            progressed("Computing correlation...", (((l+1)*(m+1)+m+1) * 100) / 9);
+            long double channelLreduced = reducer * sumChannel[l];
+            long double bothChannels = channelLreduced * sumChannel[m];
+            covariance(l, m) = sumX[l][m] - bothChannels;
         }
     }
 
     // Compute the rotation
-    Eigen::EigenSolver<Eigen::MatrixXd> solver(correlation, true);
+    Eigen::EigenSolver<Eigen::MatrixXd> solver(covariance, true);
     Eigen::MatrixXd rotation = solver.eigenvectors().real();
     Eigen::MatrixXd eigenValues = solver.eigenvalues().real();
 
+    Eigen::MatrixXd sigma = covariance.diagonal().asDiagonal();
+    for (int i=0; i<3; i++)
+        sigma(i, i) = std::sqrt(sigma(i,i));
+
+    qDebug() << eigenValues(0) << "," << eigenValues(1) << "," << eigenValues(2) << "," ;
     // Compute the stretching factor
-    eigenValues.unaryExpr([](float val){return 50 / std::sqrt(val);});
+    for (int i=0; i<3; i++)
+        eigenValues(i) = 1.0f / std::sqrt(eigenValues(i) >= 0 ? eigenValues(i) : -eigenValues(i));
+    qDebug() << eigenValues(0) << "," << eigenValues(1) << "," << eigenValues(2) << "," ;
 
     // Compute the final transformation matrix
-    Eigen::MatrixXd transformation = rotation.transpose() * eigenValues.asDiagonal() * rotation;
-    // Compute the mean of the channels
-    for (int i=0; i<3; i++)
-        means(i) /= (width*height);
+    Eigen::MatrixXd transformation = sigma * rotation * eigenValues.asDiagonal() * rotation.transpose();
     // Apply the transformation to the mean
-    Eigen::VectorXd offset = transformation * means;
+    Eigen::VectorXd offset = means - transformation * means;
 
     // Finally reposition the pixels with that offset
     decoder.init(input_folder.toStdString().c_str(), width, height);
+    Eigen::VectorXd currPixel(3);
+
     for (int i=0; i<height; i++)
     {
         decoder.readRows(1, pixels.data());
         for (int k=0; k<pixels.size(); k+=3)
         {
-            dstretched.push_back(pixels[k] + offset(0));
-            dstretched.push_back(pixels[k+1] + offset(1));
-            dstretched.push_back(pixels[k+2] + offset(2));
+            currPixel(0) = pixels[k] - means(0);
+            currPixel(1) = pixels[k+1] - means(1);
+            currPixel(2) = pixels[k+2] - means(2);
+
+            currPixel = transformation * currPixel + means;
+
+            dstretched.push_back(currPixel(0));
+            dstretched.push_back(currPixel(1));
+            dstretched.push_back(currPixel(2));
         }
     }
 
