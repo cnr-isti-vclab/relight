0e2d6994ed5e28e92f8b224de620c5c42689e895
Federico Ponchio <ponchio@gmail.com>
2023-08-22 17:05:11 +0200

    Update mac os 0_setup_env.sh for fftw

--- build_scripts/macOS/0_setup_env.sh
+++ build_scripts/macOS/0_setup_env.sh
@@ -23,7 +23,7 @@ esac
 done
 
 
-brew install coreutils llvm cmake ninja libomp eigen libjpeg
+brew install coreutils llvm cmake ninja libomp eigen libjpeg fftw
 npm install -g appdmg
 
 if [ "$DONT_INSTALL_QT" = false ] ; then
