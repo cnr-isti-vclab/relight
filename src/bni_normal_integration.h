4cdb7bf8e0f42f22efacf2b79b6aef42b0b14351
ponchio <ponchio@gmail.com>
2024-01-22 18:18:40 +0100

    command line normal integration, and changed default parameters.

--- src/bni_normal_integration.h
+++ src/bni_normal_integration.h
@@ -3,16 +3,19 @@
 
 #include <QString>
 #include <vector>
+#include <functional>
 /* Bilateral Normal Integration Xu Cao, Hiroaki Santo1, Boxin Shi, Fumio Okura1, and Yasuyuki Matsushita1
  *
  * https://github.com/xucao-42/bilateral_normal_integration
 */
 
-std::vector<double> bni_integrate(int w, int h, std::vector<float> &normalmap,
-                                double k = 2.0,
-                                double tolerance = 1e-5,
-                                int max_iterations = 150,
-                                int max_solver_iterations = 1000);
+std::vector<double> bni_integrate(std::function<bool(std::string s, int n)> progressed,
+								  int w, int h, std::vector<float> &normalmap,
+								  double k = 2.0,
+								  double tolerance = 1e-5,
+								  double solver_tolerance = 1e-5,
+								  int max_iterations = 150,
+								  int max_solver_iterations = 1000);
 
 bool savePly(const QString &filename, int w, int h, std::vector<double> &z);
 bool saveDepthMap(const QString &filename, int w, int h, std::vector<float> &z);
