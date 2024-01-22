c501ad7876acdcc7ca1ee739166519ecb4aa3c91
ponchio <ponchio@gmail.com>
2024-01-22 18:38:20 +0100

    using olve with guess to greatly improve speed.

--- src/bni_normal_integration.h
+++ src/bni_normal_integration.h
@@ -15,7 +15,7 @@ std::vector<double> bni_integrate(std::function<bool(std::string s, int n)> prog
 								  double tolerance = 1e-5,
 								  double solver_tolerance = 1e-5,
 								  int max_iterations = 150,
-								  int max_solver_iterations = 1000);
+								  int max_solver_iterations = 5000);
 
 bool savePly(const QString &filename, int w, int h, std::vector<double> &z);
 bool saveDepthMap(const QString &filename, int w, int h, std::vector<float> &z);
