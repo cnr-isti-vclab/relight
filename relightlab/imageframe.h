e33dd307bf11feba1fe2586c18726582a21a3556
ponchio <ponchio@gmail.com>
2024-01-29 18:46:52 +0100

    showing images, dealing with zooming issues.

--- relightlab/imageframe.h
+++ relightlab/imageframe.h
@@ -5,15 +5,25 @@
 
 class Canvas;
 class QStatusBar;
+class QGraphicsPixmapItem;
+class QGraphicsScene;
 
 class ImageFrame: public QFrame {
+	Q_OBJECT
 public:
+	enum Mode { SINGLE, LIST, THUMBNAILS };
 	Canvas *canvas = nullptr;
 	QStatusBar *status = nullptr;
 	ImageFrame();
 
-public slots:
+	void showImage(int id); //new project loaded.
 
+public slots:
+	void fit();  //fit image on screen
+	void one();  //scale to 1:1 zoom
+private:
+	QGraphicsPixmapItem *imagePixmap = nullptr;
+	QGraphicsScene *scene = nullptr;
 };
 
 #endif // IMAGEFRAME_H
