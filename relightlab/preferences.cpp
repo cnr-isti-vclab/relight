bfd8f3b43868d0fedc9e2840e099a795ff1f6653
ponchio <ponchio@gmail.com>
2024-01-31 20:08:10 +0100

    adding recent project list to home page.

--- relightlab/preferences.cpp
+++ relightlab/preferences.cpp
@@ -1,6 +1,28 @@
 #include "preferences.h"
+#include "tabwidget.h"
 
-Preferences::Preferences()
-{
+#include <QVBoxLayout>
+#include <QPushButton>
 
+Preferences::Preferences(QWidget *parent): QDialog(parent) {
+	setWindowTitle("Preferences - RelightLab");
+
+	tabs = new TabWidget;
+
+	QWidget *appearance = new QWidget;
+	tabs->addTab(appearance, "Appearance");
+
+	QWidget *performances = new QWidget;
+	tabs->addTab(performances, "Performances");
+
+	QWidget *casting = new QWidget;
+	tabs->addTab(casting, "Casting");
+
+	QVBoxLayout *layout = new QVBoxLayout(this);
+	layout->addWidget(tabs);
+
+	QPushButton *closeButton = new QPushButton("Close", this);
+	layout->addWidget(closeButton);
+
+	connect(closeButton, &QPushButton::clicked, this, &QDialog::accept);
 }
