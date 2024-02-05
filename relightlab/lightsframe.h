c63d32fe91f885bfa3d6021a21ea9597b8681f50
ponchio <ponchio@gmail.com>
2024-02-05 19:28:24 +0100

    lp loading.

--- relightlab/lightsframe.h
+++ relightlab/lightsframe.h
@@ -3,22 +3,7 @@
 
 #include <QStackedWidget>
 
-class LightsFrame: public QStackedWidget {
-	Q_OBJECT
-public:
-	LightsFrame();
-
-public slots:
-	void showLp();
-	void showSphere();
-	void showDome();
-
-private:
-	QFrame *createLightsChoice();
-	QFrame *createLp();
-	QFrame *createSphere();
-	QFrame *createDome();
-};
+class QLabel;
 
 class Card: public QFrame {
 	Q_OBJECT
@@ -29,6 +14,50 @@ public slots:
 signals:
 	void clicked();
 };
+
+class LpFrame: public QFrame {
+	Q_OBJECT
+public:
+	LpFrame(QWidget *parent = nullptr);
+	void init();
+	void loadLP(QString filename);
+public slots:
+	void loadLP();
+private:
+	QLabel *filename;
+};
+
+class SphereFrame: public QFrame {
+public:
+	SphereFrame(QWidget *parent = nullptr);
+	void init();
+};
+
+class DomeFrame: public QFrame {
+public:
+	DomeFrame(QWidget *parent = nullptr);
+	void init();
+};
+
+class LightsFrame: public QStackedWidget {
+	Q_OBJECT
+public:
+	LightsFrame();
+
+public slots:
+	void init();
+	void showChoice();
+	void showLp();
+	void showSphere();
+	void showDome();
+
+private:
+	QFrame *createChoiceFrame();
+	LpFrame *lp = nullptr;
+	SphereFrame *sphere = nullptr;
+	DomeFrame *dome = nullptr;
+};
+
 
 
 #endif // LIGHTSFRAME_H
