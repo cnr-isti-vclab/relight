6a85f12dd607fc80abfa847b12bda715ecafc5b1
ponchio <ponchio@gmail.com>
2024-02-09 01:15:37 +0100

    refactoring and lppanel finally done.

---
+++ relightlab/directionsview.cpp
@@ -0,0 +1,33 @@
+#include "directionsview.h"
+#include "../src/dome.h"
+
+#include <QGraphicsEllipseItem>
+#include <QDebug>
+
+DirectionsView::DirectionsView(QWidget *parent): QGraphicsView(parent) {
+	setScene(&scene);
+}
+
+void DirectionsView::initFromDome(Dome &dome) {
+	qreal scale = width();
+	qDebug() << scale;
+	//scene goes from [-1, +1]x[-1, +1], view will just zoom on it
+	qreal diameter = lightSize;
+	int count = 0;
+	for(Vector3f &dir: dome.directions) {
+		QGraphicsEllipseItem *e = scene.addEllipse(dir[0]*scale, dir[1]*scale, diameter, diameter);
+		e->setToolTip(QString::number(count++));
+		e->setBrush(Qt::white);
+	}
+
+	qreal margin = scale/10;
+	qreal side = scale + margin;
+	fitInView(QRectF(-side, -side, 2*side, 2*side), Qt::KeepAspectRatio);
+}
+
+void DirectionsView::highlight(int n) {
+	for(int i = 0; i < scene.items().size(); i++) {
+		QGraphicsEllipseItem *item = (QGraphicsEllipseItem *)(scene.items()[i]);
+		item->setBrush(i == n? Qt::red : Qt::white);
+	}
+}
---
+++ relightlab/directionsview.h
@@ -0,0 +1,24 @@
+#ifndef DIRECTIONSVIEW_H
+#define DIRECTIONSVIEW_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+
+class Dome;
+
+class DirectionsView: public QGraphicsView {
+	Q_OBJECT
+public:
+	double lightSize = 10.0;
+
+	DirectionsView(QWidget *parent = nullptr);
+	void initFromDome(Dome &dome);
+
+public slots:
+	void highlight(int n);
+
+private:
+	QGraphicsScene scene;
+};
+
+#endif // DIRECTIONSVIEW_H
