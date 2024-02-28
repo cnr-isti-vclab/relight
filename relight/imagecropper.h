c8b255380aa75ef701c5deb5bed82a09fbd2594d
ponchio <ponchio@gmail.com>
2024-02-28 17:30:07 +0100

    fixed cropping

--- relight/imagecropper.h
+++ relight/imagecropper.h
@@ -5,19 +5,17 @@
 #include <QSizeF>
 
 enum CursorPosition {
-	CursorPositionUndefined,
-	CursorPositionMiddle,
-	CursorPositionTop,
-	CursorPositionBottom,
-	CursorPositionLeft,
-	CursorPositionRight,
-	CursorPositionTopLeft,
-	CursorPositionTopRight,
-	CursorPositionBottomLeft,
-	CursorPositionBottomRight
+	CursorPositionUndefined = 0,
+	CursorPositionMiddle = 1 | 2 | 4 | 8,
+	CursorPositionLeft = 1,
+	CursorPositionTop = 2,
+	CursorPositionRight = 4,
+	CursorPositionBottom = 8,
+	CursorPositionTopLeft = 1 | 2,
+	CursorPositionTopRight = 2 | 4,
+	CursorPositionBottomLeft = 8 | 1,
+	CursorPositionBottomRight = 8 | 4
 };
-
-
 
 class ImageCropper : public QWidget {
 	Q_OBJECT
@@ -29,26 +27,31 @@ public:
 public slots:
 	void setImage(const QPixmap& _image);
 	void setCroppingRectBorderColor(const QColor& _borderColor);
+
 	void setProportion(const QSizeF& _proportion);
 	void setProportionFixed(const bool _isFixed);
+
 	void showHandle(bool _show = true);
 	void hideHandle();
-	void setCrop(QRect rect, bool preserveArea = false);
+
+	void setCrop(QRect rect);
 	void setWidth(int w);
 	void setHeight(int h);
 	void setTop(int t);
 	void setLeft(int l);
 	void resetCrop();
+	void maximizeCrop();
+	void centerCrop();
 
 signals:
 	void areaChanged(QRect rect);
 
 public:
 	bool handleShown() { return show_handle; }
-	QRect imageCroppedRect(); //return cropped rect in image ccords
+	QRectF imageCroppedRect(); //return cropped rect in image ccords
 	QRect croppedRect();
-	void enforceBounds(bool preserveArea); //makes sure the rectangle is inside the image.
-
+	void enforceBounds(QRect rect, CursorPosition position); //makes sure the rectangle is inside the image.
+	QRect ensureCropFits(QRect rect);
 protected:
 	virtual void resizeEvent(QResizeEvent *event);
 	virtual void paintEvent(QPaintEvent* _event);
@@ -59,24 +62,6 @@ protected:
 private:
 	CursorPosition cursorPosition(const QRectF& _cropRect, const QPointF& _mousePosition);
 	void updateCursorIcon(const QPointF& _mousePosition);
-
-	const QRectF calculateGeometry(
-			const QRectF& _sourceGeometry,
-			const CursorPosition _cursorPosition,
-			const QPointF& _mouseDelta
-			);
-	const QRectF calculateGeometryWithCustomProportions(
-			const QRectF& _sourceGeometry,
-			const CursorPosition _cursorPosition,
-			const QPointF& _mouseDelta
-			);
-	const QRectF calculateGeometryWithFixedProportions(
-			const QRectF &_sourceGeometry,
-			const CursorPosition _cursorPosition,
-			const QPointF &_mouseDelta,
-			const QSizeF &_deltas
-			);
-
 	void updateDeltaAndScale();
 
 
@@ -87,11 +72,12 @@ private:
 	float leftDelta = 0, topDelta = 0;
 	float xScale = 1, yScale = 1;
 
-	QPixmap imageForCropping;
-	QRectF croppingRect; //in image coords
-	QRectF realSizeRect;
+	QPixmap image;
+	//QRectF croppingRect; //in image coords //j
 
-	QRectF lastStaticCroppingRect;
+	QRect realSizeRect;
+	QRect lastStaticCroppingRect;
+
 	CursorPosition _cursorPosition = CursorPositionUndefined;
 	bool isMousePressed = false;
 	bool isProportionFixed = false;
