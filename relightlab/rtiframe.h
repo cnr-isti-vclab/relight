bcdefefc730192dff58be1a0d9b309bf5f3f5fd5
ponchio <ponchio@gmail.com>
2024-03-04 09:20:02 +0100

    rtiframe redesign

--- relightlab/rtiframe.h
+++ relightlab/rtiframe.h
@@ -3,10 +3,19 @@
 
 #include <QFrame>
 
+class QPushButton;
+
 class RtiFrame: public QFrame {
 public:
+	enum Basis { PTM = 0, HSH = 1, RBF = 2, BNL = 3, NEURAL = 4 };
+	enum Colorspace { RGB = 0, LRGB = 1, MRGB = 2, YCC = 3};
+
 	RtiFrame(QWidget *parent = nullptr);
 	void init();
+
+private:
+	QPushButton *basis[5];
+	QPushButton *spaces[4];
 };
 
 #endif // RTIFRAME_H
