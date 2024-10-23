63993b30d90b06ad58aa2b58b06a08e8355788be
ponchio <ponchio@gmail.com>
2024-10-24 01:03:10 +0200

    minor normal refactoring and relight-cli computes also per pixel normals

--- relight/normalstask.cpp
+++ relight/normalstask.cpp
@@ -26,6 +26,56 @@ using namespace std;
 ///         That NormalsWorker fills a vector with the colors of the normals in that line.
 ///
 
+void saveNormals(NormalSolver solver, ImageSet &imageSet, QString output, std::function<bool(std::string s, int d)> *callback) {
+	std::vector<float> normals;
+	computeNormals(NORMALS_L2, imageSet, normals, callback);
+	std::vector<uint8_t> normalmap(imageSet.width * imageSet.height * 3);
+
+	for(size_t i = 0; i < normals.size(); i++)
+		normalmap[i] = floor(((normals[i] + 1.0f) / 2.0f) * 255);
+
+	QImage img(normalmap.data(), imageSet.width, imageSet.height, imageSet.width*3, QImage::Format_RGB888);
+	img.save(output);
+}
+
+void computeNormals(NormalSolver solver, ImageSet &imageSet, std::vector<float> &normals, std::function<bool(std::string s, int d)> *callback) {
+
+	normals.resize(imageSet.width * imageSet.height * 3);
+	RelightThreadPool pool;
+	PixelArray line;
+
+	pool.start(QThread::idealThreadCount());
+
+	for (int i=0; i<imageSet.height; i++) {
+		// Read a line
+		imageSet.readLine(line);
+
+		// Create the normal task and get the run lambda
+		uint32_t idx = i * 3 * imageSet.width;
+		//uint8_t* data = normals.data() + idx;
+		float* data = &normals[idx];
+
+		NormalsWorker *task = new NormalsWorker(solver, i, line, data, imageSet);
+
+		std::function<void(void)> run = [task](void)->void {
+			task->run();
+			delete task;
+		};
+
+		pool.queue(run);
+		pool.waitForSpace();
+
+		if(! (*callback)("Computing normals...", ((float)i / imageSet.height) * 100)) {
+			break;
+		}
+	}
+
+	// Wait for the end of all the threads
+	pool.finish();
+
+
+}
+
 void NormalsTask::run() {
 	status = RUNNING;
 	QList<QVariant> qlights = (*this)["lights"].value.toList();
@@ -51,93 +101,65 @@ void NormalsTask::run() {
 		imageSet.crop(rect.left(), rect.top(), rect.width(), rect.height());
 	}
 
-// Normals vector
+	// Normals vector
 
 	int start = clock();
 	imageSet.setCallback(nullptr);
 
+	std::function<bool(std::string s, int d)> callback = [this](std::string s, int n)->bool { return this->progressed(s, n); };
 
 
-	std::vector<float> normals(imageSet.width * imageSet.height * 3);
+	std::vector<float> normals;
+	computeNormals(solver, imageSet, normals, &callback);
 
-	RelightThreadPool pool;
-	PixelArray line;
+	std::vector<uint8_t> normalmap(imageSet.width * imageSet.height * 3);
+	for(size_t i = 0; i < normals.size(); i++)
+		normalmap[i] = floor(((normals[i] + 1.0f) / 2.0f) * 255);
 
-	pool.start(QThread::idealThreadCount());
-
-	for (int i=0; i<imageSet.height; i++) {
-        // Read a line
-        imageSet.readLine(line);
-
-        // Create the normal task and get the run lambda
-        uint32_t idx = i * 3 * imageSet.width;
-        //uint8_t* data = normals.data() + idx;
-        float* data = &normals[idx];
-
-		NormalsWorker *task = new NormalsWorker(solver, i, line, data, imageSet);
-
-		std::function<void(void)> run = [this, task](void)->void {
-			task->run();
-			delete task;
-		};
-
-		pool.queue(run);
-		pool.waitForSpace();
-
-		progressed("Computing normals...", ((float)i / imageSet.height) * 100);
+	// Save the final result
+	QImage img(normalmap.data(), imageSet.width, imageSet.height, imageSet.width*3, QImage::Format_RGB888);
+	// Set spatial resolution if known. Need to convert as pixelSize stored in mm/pixel whereas QImage requires pixels/m
+	if( pixelSize > 0 ) {
+		int dotsPerMeter = round(1000.0/pixelSize);
+		img.setDotsPerMeterX(dotsPerMeter);
+		img.setDotsPerMeterY(dotsPerMeter);
 	}
+	img.save(output);
 
-    // Wait for the end of all the threads
-    pool.finish();
 
-    std::vector<uint8_t> normalmap(imageSet.width * imageSet.height * 3);
-    for(size_t i = 0; i < normals.size(); i++)
-        normalmap[i] = floor(((normals[i] + 1.0f) / 2.0f) * 255);
-
-    // Save the final result
-    QImage img(normalmap.data(), imageSet.width, imageSet.height, imageSet.width*3, QImage::Format_RGB888);
-    // Set spatial resolution if known. Need to convert as pixelSize stored in mm/pixel whereas QImage requires pixels/m
-    if( pixelSize > 0 ) {
-        int dotsPerMeter = round(1000.0/pixelSize);
-        img.setDotsPerMeterX(dotsPerMeter);
-        img.setDotsPerMeterY(dotsPerMeter);
-    }
-    img.save(output);
-    std::function<bool(std::string s, int d)> callback = [this](std::string s, int n)->bool { return this->progressed(s, n); };
-
-    if(exportSurface) {
-        progressed("Integrating normals...", 0);
+	if(exportSurface) {
+		progressed("Integrating normals...", 0);
 		std::vector<float> z;
 		bni_integrate(callback, imageSet.width, imageSet.height, normals, z, exportK);
-        if(z.size() == 0) {
-            error = "Failed to integrate normals";
-            status = FAILED;
-            return;
-        }
-        QString filename = output.left(output.size() -4) + ".ply";
+		if(z.size() == 0) {
+			error = "Failed to integrate normals";
+			status = FAILED;
+			return;
+		}
+		QString filename = output.left(output.size() -4) + ".ply";
 
-        progressed("Saving surface...", 99);
-        savePly(filename, imageSet.width, imageSet.height, z);
-    }
-    int end = clock();
-    qDebug() << "Time: " << ((double)(end - start) / CLOCKS_PER_SEC);
-    progressed("Finished", 100);
+		progressed("Saving surface...", 99);
+		savePly(filename, imageSet.width, imageSet.height, z);
+	}
+	int end = clock();
+	qDebug() << "Time: " << ((double)(end - start) / CLOCKS_PER_SEC);
+	progressed("Finished", 100);
 }
 
 bool NormalsTask::progressed(std::string s, int percent)
 {
-    if(status == PAUSED) {
-        mutex.lock();  //mutex should be already locked. this talls the
-        mutex.unlock();
-    }
-    if(status == STOPPED)
-        return false;
+	if(status == PAUSED) {
+		mutex.lock();  //mutex should be already locked. this talls the
+		mutex.unlock();
+	}
+	if(status == STOPPED)
+		return false;
 
-    QString str(s.c_str());
-    emit progress(str, percent);
-    if(status == STOPPED)
-        return false;
-    return true;
+	QString str(s.c_str());
+	emit progress(str, percent);
+	if(status == STOPPED)
+		return false;
+	return true;
 }
 
 /**
@@ -148,48 +170,49 @@ bool NormalsTask::progressed(std::string s, int percent)
 
 void NormalsWorker::run()
 {
-    switch (solver)
-    {
-    // L2 solver
-    case NORMALS_L2:
-        solveL2();
-        break;
-    // SBL solver
-    case NORMALS_SBL:
-        solveSBL();
-        break;
-    // RPCA solver
-    case NORMALS_RPCA:
-        solveRPCA();
-        break;
-    }
+	switch (solver)
+	{
+	// L2 solver
+	case NORMALS_L2:
+		solveL2();
+		break;
+		// SBL solver
+	case NORMALS_SBL:
+		solveSBL();
+		break;
+		// RPCA solver
+	case NORMALS_RPCA:
+		solveRPCA();
+		break;
+	}
 
-    // Deallocate line (TODO: useless?)
-    std::vector<Pixel>().swap(m_Row);
+	// Deallocate line (TODO: useless?)
+	std::vector<Pixel>().swap(m_Row);
 }
+
 
 
 void NormalsWorker::solveL2()
 {
 	std::vector<Vector3f> &m_Lights = m_Imageset.lights;
 	std::vector<Vector3f> &m_Lights3d = m_Imageset.lights3d;
-    // Pixel data
+	// Pixel data
 	Eigen::MatrixXd mLights(m_Lights.size(), 3);
 	Eigen::MatrixXd mPixel(m_Lights.size(), 1);
-    Eigen::MatrixXd mNormals;
+	Eigen::MatrixXd mNormals;
 
-    unsigned int normalIdx = 0;
+	unsigned int normalIdx = 0;
 
 
-    // Fill the lights matrix
+	// Fill the lights matrix
 	for (size_t i = 0; i < m_Lights.size(); i++)
 		for (int j = 0; j < 3; j++)
-            mLights(i, j) = m_Lights[i][j];
+			mLights(i, j) = m_Lights[i][j];
 
-    // For each pixel in the line solve the system
-    //TODO do it in a single pass, it's faster.
+	// For each pixel in the line solve the system
+	//TODO do it in a single pass, it's faster.
 	for (size_t p = 0; p < m_Row.size(); p++) {
-        // Fill the pixel vector
+		// Fill the pixel vector
 		for (size_t m = 0; m < m_Lights.size(); m++)
 			mPixel(m, 0) = m_Row[p][m].mean();
 
--- relight/normalstask.h
+++ relight/normalstask.h
@@ -16,50 +16,51 @@
 
 enum NormalSolver { NORMALS_L2, NORMALS_SBL, NORMALS_RPCA };
 
+void saveNormals(NormalSolver, ImageSet &imageSet, QString output, std::function<bool(std::string s, int d)> *callback = nullptr );
+void computeNormals(NormalSolver solver, ImageSet &imageSet, std::vector<float> &normals, std::function<bool(std::string s, int d)> *callback = nullptr);
 
-class NormalsTask :  public Task
-{
+
+class NormalsTask :  public Task {
 public:
-    NormalSolver solver;
-    bool exportSurface = false;
-    bool exportK = 2.0;
-    QRect m_Crop;
-    float pixelSize = 0;
+	NormalSolver solver;
+	bool exportSurface = false;
+	bool exportK = 2.0;
+	QRect m_Crop;
+	float pixelSize = 0;
 
 	NormalsTask(Project *_project, NormalSolver _solver) :
 		project(_project), solver(_solver){
 	}
 	virtual ~NormalsTask() {};
 
-    virtual void run() override;
+	virtual void run() override;
 
 public slots:
-    bool progressed(std::string str, int percent) override;
+	bool progressed(std::string str, int percent) override;
 
 private:
 	//TODO remove dependency on project!
 	Project *project;
-
 };
 
 class NormalsWorker
 {
 public:
 	NormalsWorker(NormalSolver _solver, int _row, PixelArray& toProcess, float* normals, ImageSet &imageset) :
-		 solver(_solver), row(_row), m_Row(toProcess), m_Normals(normals), m_Imageset(imageset){}
+		solver(_solver), row(_row), m_Row(toProcess), m_Normals(normals), m_Imageset(imageset){}
 
-    void run();
+	void run();
 private:
-    void solveL2();
-    void solveSBL();
-    void solveRPCA();
+	void solveL2();
+	void solveSBL();
+	void solveRPCA();
 private:
 
-    NormalSolver solver;
+	NormalSolver solver;
 	int row;
-    PixelArray m_Row;
-    //uint8_t* m_Normals;
-    float* m_Normals;
+	PixelArray m_Row;
+	//uint8_t* m_Normals;
+	float* m_Normals;
 	ImageSet &m_Imageset;
-    QMutex m_Mutex;
+	QMutex m_Mutex;
 };
