fe84b3db3ebba989fe3f43a92d6d1b73814b308a
ponchio <ponchio@gmail.com>
2024-11-12 03:45:14 +0100

    added some checks on sphere picking.

--- relightlab/spheredialog.cpp
+++ relightlab/spheredialog.cpp
@@ -1,9 +1,11 @@
 #include "spheredialog.h"
 #include "spherepicking.h"
 #include "imageview.h"
+#include "../src/sphere.h"
 
 #include <QVBoxLayout>
 #include <QDialogButtonBox>
+#include <QMessageBox>
 
 SphereDialog::SphereDialog(QWidget *parent): QDialog(parent) {
 	setModal(true);
@@ -28,6 +30,15 @@ void SphereDialog::setSphere(Sphere *sphere) {
 
 
 void SphereDialog::accept() {
+	if(sphere_picking->sphere->border.size() < 3) {
+		QMessageBox::information(this, "Not enought points", "At least 3 points are needed to fit a circle");
+		return;
+	}
+	if(!sphere_picking->sphere->fitted) {
+		QMessageBox::information(this, "Sphere not fit", "The sphere could not be fit, try to remove and add back the points");
+		return;
+	}
+	//TODO check for a valid (inside the image) inner rectangle.
 	QDialog::accept();
 }
 
