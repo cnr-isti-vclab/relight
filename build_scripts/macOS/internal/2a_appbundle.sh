fd765772cfbc477b50e42977efc7f2601147ea5d
alemuntoni <alemuntoni@users.noreply.github.com>
2024-11-20 13:30:25 +0100

    fix mac deploy script

--- build_scripts/macOS/internal/2a_appbundle.sh
+++ build_scripts/macOS/internal/2a_appbundle.sh
@@ -24,7 +24,25 @@ case $i in
 esac
 done
 
-${QT_DIR}macdeployqt $INSTALL_PATH/$APPNAME
+QT_BASE_DIR=""
+
+# if QT_DIR is not empty
+if [ -n "$QT_DIR" ]; then
+    # set QT_BASE_DIR to the path of QT_DIR/bin
+    QT_BASE_DIR="${QT_DIR}/bin/"
+fi
+
+# save in message the output of macdeployqt
+message=$(${QT_BASE_DIR}macdeployqt $INSTALL_PATH/$APPNAME \
+    $ARGUMENTS 2>&1)
+
+# if message contains "ERROR" then macdeployqt failed
+if [[ $message == *"ERROR"* ]]; then
+    echo "macdeployqt failed."
+    echo "macdeployqt output:"
+    echo $message
+    exit 1
+fi
 
 # remove everything from install path, except the appbundle
 cd $INSTALL_PATH
