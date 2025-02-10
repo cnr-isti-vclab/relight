5b95cbef9bf95ef0906e9f37649dfd8632d7e2c9
ponchio <ponchio@gmail.com>
2025-02-11 00:41:23 +0100

    typo

--- build_scripts/macOS/0_setup_env.sh
+++ build_scripts/macOS/0_setup_env.sh
@@ -6,38 +6,7 @@
 #
 # Requires: homebrew
 
-DONT_INSTALL_QT=false
-
-#checking for parameters
-for i in "$@"
-do
-case $i in
-    --dont_install_qt)
-        DONT_INSTALL_QT=true
-        shift # past argument=value
-        ;;
-    *)
-        # unknown option
-        ;;
-esac
-done
-
-
 brew install coreutils cmake ninja libomp eigen libjpeg libtiff dylibbundler #opencv
 
 npm install -g appdmg
 
-
-#if [ "$DONT_INSTALL_QT" = false ] ; then
-#    echo "=== installing qt packages..."
-
-#    brew install qt
-
-#    echo "export QTDIR=/usr/local/opt/qt" >> ~/.bash_profile
-#    echo "export PATH=$QTDIR/bin:$PATH" >> ~/.bash_profile
-#    echo "export LD_LIBRARY_PATH=/usr/local/opt/qt/lib:$LD_LIBRARY_PATH" >> ~/.bash_profile
-#    echo "export PKG_CONFIG_PATH=/usr/local/opt/qt/lib:$PKG_CONFIG_PATH" >> ~/.bash_profile
-#    . ~/.bash_profile
-#else
-#    echo "=== jumping installation of qt packages..."
-#fi
--- build_scripts/macOS/internal/2a_appbundle.sh
+++ build_scripts/macOS/internal/2a_appbundle.sh
@@ -36,7 +36,7 @@ fi
 message=$(${QT_BASE_DIR}macdeployqt $INSTALL_PATH/$APPNAME \
     $ARGUMENTS 2>&1)
 
-dylibbundler $INSTALL_PATH/$APPNAME/Content/MacOs/%{APPNAME%.app} $INSTALL_PATH/$APPNAME/Framework @executable_path/../Framework
+dylibbundler $INSTALL_PATH/$APPNAME/Content/MacOS/${APPNAME%.app} $INSTALL_PATH/$APPNAME/Framework @executable_path/../Framework
 
 # if message contains "ERROR" then macdeployqt failed
 if [[ $message == *"ERROR"* ]]; then
