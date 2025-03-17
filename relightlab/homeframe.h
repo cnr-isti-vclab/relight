3b9215f8439f79db3b4fea4bd1d2e207f30263cd
Federico Ponchio <ponchio@gmail.com>
2025-03-17 18:24:21 +0100

    Docs moved from md to html.

--- relightlab/homeframe.h
+++ relightlab/homeframe.h
@@ -2,10 +2,18 @@
 #define HOMEWIDGET_H
 
 #include <QFrame>
+#include <QTextBrowser>
 
 class HomeFrame: public QFrame {
 public:
 	HomeFrame();
+};
+
+class HelpBrowser: public QTextBrowser {
+public:
+	HelpBrowser(QWidget *parent = nullptr): QTextBrowser(parent) {}
+public slots:
+	void doSetSource(const QUrl &url, QTextDocument::ResourceType type = QTextDocument::UnknownResource);
 };
 
 #endif // HOMEWIDGET_H
