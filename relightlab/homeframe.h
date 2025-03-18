24a8ba0a3fbade67c8b4ecb80793d33249cb6957
Federico Ponchio <ponchio@gmail.com>
2025-03-18 01:07:35 +0100

    Working on the documentation.

--- relightlab/homeframe.h
+++ relightlab/homeframe.h
@@ -10,9 +10,18 @@ public:
 };
 
 class HelpBrowser: public QTextBrowser {
+	Q_OBJECT
 public:
-	HelpBrowser(QWidget *parent = nullptr): QTextBrowser(parent) {}
+	HelpBrowser(QWidget *parent = nullptr): QTextBrowser(parent) {
+		setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+	}
+
+	QStringList history;
+	int current = -1;
 public slots:
+	void next();
+	void previous();
+	//unfortunately QTextBrowser offer no direct access to history and setHtml erases it.
 	void doSetSource(const QUrl &url, QTextDocument::ResourceType type = QTextDocument::UnknownResource);
 };
 
