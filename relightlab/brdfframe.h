7feaf91f1ca42d23716f2a8e438b2fd64cd54c2e
ponchio <ponchio@gmail.com>
2025-05-20 10:58:08 +0200

    adding brdf tab

--- relightlab/brdfframe.h
+++ relightlab/brdfframe.h
@@ -2,18 +2,27 @@
 #define BRDFFRAME_H
 
 #include <QFrame>
+#include "planrow.h"
+
+class BrdfMedianRow: public PlanRow {
+public:
+	BrdfMedianRow(QWidget *parent = nullptr);
+};
 
 class BrdfFrame: public QFrame {
 	Q_OBJECT
-	public:
-		BrdfFrame(QWidget *parent = nullptr);
-		void clear();
-		void init();
+public:
+	BrdfFrame(QWidget *parent = nullptr);
+	void clear();
+	void init();
 
-	public slots:
+public slots:
 
+signals:
+	void processStarted();
 
-	private:
-	};
+private:
+	BrdfMedianRow *median_row = nullptr;
+};
 
 #endif // BRDFFRAME_H
