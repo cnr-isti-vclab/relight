ffb84f9b10135227391cc7b25a00be9e2d573328
ponchio <ponchio@gmail.com>
2025-07-07 16:09:41 +0200

    range for depthmap added and only for normalized.

--- src/bni_normal_integration.cpp
+++ src/bni_normal_integration.cpp
@@ -159,12 +159,15 @@ bool saveTiff(const QString &filename, size_t w, size_t h, std::vector<float> &d
 	}
 
 	TIFFClose(outTiff);
-	//if normalize: save range in mm and pixel size (in mm) in a text file.
-	QFile file(filename.left(filename.size() -4) + ".txt");
-	file.open(QFile::WriteOnly);
-	QTextStream stream(&file);
-	stream << "min: " << min << "\n";
-	stream << "max: " << max << "\n";
+	if(normalize) {
+		//if normalize: save range in mm and pixel size (in mm) in a text file.
+		QFile file(filename.left(filename.size() -4) + ".txt");
+		file.open(QFile::WriteOnly);
+		QTextStream stream(&file);
+		stream << "min: " << min << "\n";
+		stream << "max: " << max << "\n";
+		stream << "range: " << max - min << "\n";
+	}
 	return true;
 }
 
