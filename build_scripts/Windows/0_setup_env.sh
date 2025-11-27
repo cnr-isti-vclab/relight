cefb9c9d2d727fd61b80d7761c6500ddd57dc5aa
Federico Ponchio <ponchio@gmail.com>
2025-11-28 00:29:32 +0100

    Remove Qt installation from Windows setup script (handled separately)

--- build_scripts/Windows/0_setup_env.sh
+++ build_scripts/Windows/0_setup_env.sh
@@ -4,33 +4,8 @@
 #
 # Run this script if you never installed any of the RelightLab dependencies.
 #
-# Requires: choco
-
-DONT_INSTALL_QT=false
-
-#checking for parameters
-for i in "$@"
-do
-case $i in
-    --dont_install_qt)
-        DONT_INSTALL_QT=true
-        shift # past argument=value
-        ;;
-    *)
-        # unknown option
-        ;;
-esac
-done
-
-choco install cmake ninja ccache wget nsis
-
-if [ "$DONT_INSTALL_QT" = false ] ; then
-    echo "=== installing qt packages..."
-
-    choco install qt5-default
-else
-    echo "=== jumping installation of qt packages..."
-fi
+# Requires: vcpkg
+# Note: Qt should be installed separately (e.g., via the Qt installer or GitHub Actions)
 
 # Install vcpkg if not already installed
 if [ ! -d "C:/vcpkg" ]; then
@@ -39,6 +14,6 @@ if [ ! -d "C:/vcpkg" ]; then
     C:/vcpkg/bootstrap-vcpkg.bat
 fi
 
-# Install lcms2 via vcpkg
-echo "=== Installing lcms2 via vcpkg..."
+# Install dependencies via vcpkg
+echo "=== Installing dependencies via vcpkg..."
 C:/vcpkg/vcpkg.exe install lcms:x64-windows
