18d4e6d47fecb9fdeff729c10f7fc8f7b44977d2
ponchio <ponchio@gmail.com>
2025-12-15 15:24:04 +0100

    refactoring github actions

--- build_scripts/Windows/0_setup_env.sh
+++ build_scripts/Windows/0_setup_env.sh
@@ -13,10 +13,10 @@ echo "=== Installing dependencies via vcpkg..."
 C:/vcpkg/vcpkg.exe install lcms:x64-windows 
 
 #wget is needed for Windows redistributable
-C:/vcpkg/vcpkg.exe install wget:x64-windows
+#C:/vcpkg/vcpkg.exe install wget:x64-windows
 
 #for windows installer
-C:/vcpkg/vcpkg.exe install nsis:x64-windows
+#C:/vcpkg/vcpkg.exe install nsis:x64-windows
 
 #not needed for the moment.
 #C:/vcpkg/vcpkg.exe installopencv4:x64-windows
