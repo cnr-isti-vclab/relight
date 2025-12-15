8302624c2773e4d621e6357118b30cf4ee508b29
ponchio <ponchio@gmail.com>
2025-12-15 13:38:49 +0100

    Cleanup QT_VERSION in actions.

--- build_scripts/Windows/0_setup_env.sh
+++ build_scripts/Windows/0_setup_env.sh
@@ -11,14 +11,12 @@ set -euo pipefail
 echo "=== Installing dependencies via vcpkg..."
 
 C:/vcpkg/vcpkg.exe install lcms:x64-windows 
-#C:/vcpkg/vcpkg.exe install ccache:x64-windows 
-
 
 #wget is needed for Windows redistributable
-install wget:x64-windows
+C:/vcpkg/vcpkg.exe install wget:x64-windows
 
 #for windows installer
-C:/vcpkg/vcpkg.exe nsis:x64-windows
+C:/vcpkg/vcpkg.exe install nsis:x64-windows
 
 #not needed for the moment.
 #C:/vcpkg/vcpkg.exe installopencv4:x64-windows
